b674a4da720a79e0238512752111dd52bc13466e
Markus Weiss <markus.weiss@unterschiedundmacher.com>
2022-05-09 10:09:25 +0200

    add connection string

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,11 +1,15 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-	host: process.env.DB_HOST || 'localhost',
-	port: process.env.DB_PORT as unknown as number || 5455, //localTestPort
-	user: process.env.DB_USER || 'postgres', //localTestUser
-	password: process.env.DB_PASSWORD || '123456789', //localTestPassword
-	database: process.env.DB_DATABASE || 'postgresDB' //localTestDB
+	//host: process.env.DB_HOST || 'localhost',
+	//port: process.env.DB_PORT as unknown as number || 5455, //localTestPort
+	//user: process.env.DB_USER || 'postgres', //localTestUser
+	//password: process.env.DB_PASSWORD || '123456789', //localTestPassword
+	//database: process.env.DB_DATABASE || 'postgresDB', //localTestDB
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false
+	}
 	
 });
 
